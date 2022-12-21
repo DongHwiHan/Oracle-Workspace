9903919cb632d7020ad76bdb49381628ecdcfacd
HanDofasdsa <117423329+HanDofasdsa@users.noreply.github.com>
2022-12-21 13:54:30 +0900

    DB

---
+++ 07_DML(INSERT,UPDATE,DELETE)_KH계정.sql
@@ -0,0 +1,517 @@
+/*
+    DML ( DATA MANIPULATION LANGUAGE)
+    
+    데이터 조작 언어
+    
+    테이블에 새로운 데이터를 상비(INSERT)하거나
+    기존의 데이터를 수정(UPDATE) 하거나
+    삭제(DELETE)하는 구문
+*/
+
+/*
+    1. INSERT : 테이블에 새로운 행을 추가하는 구문.
+    
+    [표현법]
+    
+    INSERT INTO ~~
+    
+    1) INSERT INTO 테이블명 VALUES (값,값2,값3 ...);
+    => 해당 테이블에 "모든"칼럼에 대해 추가하고자 하는 값을 내가
+    직접 제시해서 "한 행"씩 INSERT 할때 쓰는 방법.
+    주의사항 : 컬럼의 순서, 자료형, 갯수를 맞춰서 VALUES괄호 안에 값을 나열해야함.
+    - 부족하게 제시했을경우 : 에러발생 (칼럼의 갯수가 부족)
+    - 값을 더 많이 제시한경우 : 에러발생 (TOO MANY VALUES)
+*/
+
+-- EMPLOYEE 테이블에 사원 정보추가
+INSERT INTO EMPLOYEE VALUES (
+    900,
+    '홍길동',
+    '001213-3155674',
+    'hong@kh.or.kr',
+    '01099999999',
+    'D1',
+    'J7',
+    'S6',
+    2200000,
+    NULL,
+    NULL,
+    SYSDATE,
+    NULL,
+    'N'
+);
+
+INSERT INTO EMPLOYEE VALUES (
+    901,
+    '홍길동',
+    '001213-3155674',
+    'hong@kh.or.kr',
+    '01099999999',
+    'D1',
+    'J7',
+    'S6',
+    2200000,
+    NULL,
+    NULL,
+    SYSDATE,
+    NULL, DEFAULT
+); -- DEFAULT 설정값 추가
+
+SELECT
+    *
+FROM
+    EMPLOYEE;
+
+/*
+    2) INSERT INTO 테이블명(컬럼명1, 컬럼명2, 컬럼명3) VALUES(값1, 값2, 값3);
+    => 해당 테이블에 특정 칼럼만 선택해서
+        그 칼럼에 추가할 값만 제시하고자 할때 사용
+        
+        - 그래도 한행 단위로 추가되기때문에 선택이 안된칼럼은 기본적으로 NULL OR DEFAULT 값이 들어감.
+        
+        주의 사항 : NOT NULL 제약조건 혹은 PRIMARY KEY 제약조건이 걸려있는 칼럼은 반드시 직접 값을 넣어 줘야한다.
+                    예외사항으로 NOT NULL + DEFAULT는 예외
+*/
+INSERT INTO EMPLOYEE (
+    EMP_ID,
+    EMP_NAME,
+    EMP_NO,
+    JOB_CODE,
+    SAL_LEVEL
+) VALUES (
+    902,
+    '민경민',
+    '990505-1111111',
+    'J7',
+    'S6'
+);
+
+SELECT
+    *
+FROM
+    EMPLOYEE;
+
+/*
+    3 ) INSERT INTO 테이블명 (서브쿼리);
+    => VALUES()로 값을 직접 기술하는게 아니라, 서브쿼리로 조회한 결과값을 통째로 INSERT하는 구문.
+    즉, 여러행을 한번에 INSERT할수 있음.
+*/
+
+-- 새로운 테이블
+CREATE TABLE BOARD_IMAGE (
+    BOARD_IMAGE_NO NUMBER PRIMARY KEY,
+    ORIGIN_NAME    VARCHAR2(100) NOT NULL,
+    CHANGE_NAME    VARCHAR2(100) NOT NULL
+);
+
+INSERT INTO BOARD_IMAGE
+    (
+        SELECT
+            2                   AS BOARD_IMAGE_NO,
+            'ABC.JPG'           AS ORIGIN_NAME,
+            '2022122012345.JPG' AS CHANGE_NAME
+        FROM
+            DUAL
+        UNION
+        SELECT
+            3                   AS BOARD_IMAGE_NO,
+            'ABC.JPG'           AS ORIGIN_NAME,
+            '2022122012345.JPG' AS CHANGE_NAME
+        FROM
+            DUAL
+        UNION
+        SELECT
+            4                   AS BOARD_IMAGE_NO,
+            'ABC.JPG'           AS ORIGIN_NAME,
+            '2022122012345.JPG' AS CHANGE_NAME
+        FROM
+            DUAL
+        UNION
+        SELECT
+            5                   AS BOARD_IMAGE_NO,
+            'ABC.JPG'           AS ORIGIN_NAME,
+            '2022122012345.JPG' AS CHANGE_NAME
+        FROM
+            DUAL
+    );
+
+SELECT
+    *
+FROM
+    BOARD_IMAGE;
+
+/*
+    INSERT ALL 계열
+    두 개 이상의 테이블에 각각 INSERT할때 사용
+    조건 : 그때 사용되는 서브쿼리가 동일해야한다.
+    
+    1) INSERT ALL
+        INTO 테이블1 VALUES(값들 나열)
+        INTO 테이블2 VALUES(값들 나열)
+*/
+
+-- 새로운 테이블 만들기.
+-- 첫번째 테이블 : 급여가 300만원 이상인 사원들의 사번, 사원명, 직급명을 보관할 테이블
+-- 테이블명 : EMP_JOB / EMP_ID, EMP_NAME, JOB_NAME
+CREATE TABLE EMP_JOB (
+    EMP_ID   NUMBER,
+    EMP_NAME VARCHAR2(20),
+    JOB_NAME VARCHAR2(20)
+);
+
+-- 두번째 테이블 : 급여가 300만원 이상인 사원들의 사번, 사원명, 부서명을 보관할 테이블
+-- 테이블명 : EMP_DEPT / EMP_ID, EMP_NAME, DEPT_TITLE
+CREATE TABLE EMP_DEPT (
+    EMP_ID     NUMBER,
+    EMP_NAME   VARCHAR2(20),
+    DEPT_TITLE VARCHAR2(20)
+);
+
+-- 급여가 300만원 이상인 사원들의 사번, 이름, 직급명, 부서명을 조회
+SELECT
+    EMP_ID,
+    EMP_NAME,
+    JOB_NAME,
+    DEPT_TITLE
+FROM
+         EMPLOYEE
+    JOIN JOB USING ( JOB_CODE )
+    JOIN DEPARTMENT ON ( DEPT_CODE = DEPT_ID )
+WHERE
+    SALARY >= 3000000;
+
+INSERT ALL INTO EMP_JOB VALUES (
+    EMP_ID,
+    EMP_NAME,
+    JOB_NAME
+) INTO EMP_DEPT VALUES (
+    EMP_ID,
+    EMP_NAME,
+    DEPT_TITLE
+) SELECT
+        EMP_ID,
+        EMP_NAME,
+        JOB_NAME,
+        DEPT_TITLE
+    FROM
+             EMPLOYEE
+        JOIN JOB USING ( JOB_CODE )
+        JOIN DEPARTMENT ON ( DEPT_CODE = DEPT_ID )
+  WHERE
+      SALARY >= 3000000;
+
+SELECT
+    *
+FROM
+    EMP_JOB;
+
+SELECT
+    *
+FROM
+    EMP_DEPT;
+
+/*
+    2) INSERT ALL
+    WHEN 조건1 THEN
+            INTO 테이블명1 VALUES(컬럼명)
+    WHEN 조건2 THEN
+            INTO 테이블명2 VALUES(컬럼명)
+        서브쿼리
+        - 조건에 맞는 값만 넣고 싶을때
+*/
+-- 테스트용 새로운 테이블 생성
+-- 2010년도 기준으로 이전에 입사한 사원들의 사번, 사원명, 입사일, 급여를 담는 테이블(EMP_OLD)
+CREATE TABLE EMP_OLD
+    AS
+        SELECT
+            EMP_ID,
+            EMP_NAME,
+            HIRE_DATE,
+            SALARY
+        FROM
+            EMPLOYEE
+        WHERE
+            1 = 0;
+
+-- 2010년도 기준으로 이후에 입사한 사원들의 사번, 사원명, 입사일, 급여를 담는 테이블(EMP_NEW)
+CREATE TABLE EMP_NEW
+    AS
+        SELECT
+            EMP_ID,
+            EMP_NAME,
+            HIRE_DATE,
+            SALARY
+        FROM
+            EMPLOYEE
+        WHERE
+            1 = 0;
+
+-- 1) 서브쿼리 부분
+-- 2010년 이전, 이후
+SELECT
+    EMP_ID,
+    EMP_NAME,
+    HIRE_DATE,
+    SALARY
+FROM
+    EMPLOYEE
+WHERE
+    HIRE_DATE >= '2010/01/01'; -- 2010년도 이후 입사자 10명 출력
+
+INSERT
+    ALL WHEN HIRE_DATE < '2010/01/01' THEN
+        INTO EMP_OLD (
+            EMP_ID,
+            EMP_NAME,
+            HIRE_DATE,
+            SALARY
+        )
+        WHEN HIRE_DATE >= '2010/01/01' THEN
+            INTO EMP_NEW (
+                EMP_ID,
+                EMP_NAME,
+                HIRE_DATE,
+                SALARY
+            )
+SELECT
+    EMP_ID,
+    EMP_NAME,
+    HIRE_DATE,
+    SALARY
+FROM
+    EMPLOYEE;
+
+SELECT
+    *
+FROM
+    EMP_OLD; -- 15
+SELECT
+    *
+FROM
+    EMP_NEW; -- 10
+
+DELETE FROM EMP_OLD;
+
+DELETE FROM EMP_NEW;
+
+/*
+    UPDATE
+    
+    테이블에 기록된 기존의 데이터를 수정하는 구문.
+    [표현법]
+    UPDATE 테이블명
+    SET 칼럼명 = 바꿀값
+        ,칼럼명 = 바꿀값
+        ,칼럼명 = 바꿀값 -- 여러개의 값을 동시에 변경가능.(,로 변경할 값들을 나열해야함 / AND 아님)
+        ...
+    WHERE 조건; -- WHERE 생략가능, 다만 생략하게되면 해당테이블의 "모든"행의 데이터가 바뀜.
+*/
+-- 복사본 테이블을 만든후 작업하기
+CREATE TABLE DEPT_COPY
+    AS
+        SELECT
+            *
+        FROM
+            DEPARTMENT;
+
+SELECT
+    *
+FROM
+    DEPT_COPY;
+
+-- 카피테이블에 D9부서의 부서명을 전략기획팀으로 수정
+
+UPDATE DEPT_COPY
+SET
+    DEPT_TITLE = '전략기획팀'; -- 9개행의 데이터가 모두바뀜.
+-- 전체 행의 모든 DEPT_TITLE칼럼이 전략기획팀으로 수정됨.
+
+--참고 ) 변경사항에 대해서 되돌리는 명령어 : ROLLBACK
+ROLLBACK;
+
+UPDATE DEPT_COPY
+SET
+    DEPT_TITLE = '전략기획팀'
+WHERE
+    DEPT_ID = 'D9';
+
+COMMIT;
+
+-- 복사본
+-- 테이블명 : EMP_SALARY / 칼럼 : EMPLOYEE 테이블에서 EMP_ID, EMP_NAME, DEPT_CODE, SALARY, BONUS(값도 함께)
+CREATE TABLE EMP_SALARY
+    AS
+        SELECT
+            EMP_ID,
+            EMP_NAME,
+            DEPT_CODE,
+            SALARY,
+            BONUS
+        FROM
+            EMPLOYEE;
+
+SELECT
+    *
+FROM
+    EMP_SALARY;
+
+UPDATE EMP_SALARY
+SET
+    SALARY = 10000000
+WHERE
+    EMP_NAME = '노옹철';
+
+UPDATE EMP_SALARY
+SET
+    SALARY = 70000000,
+    BONUS = NULL
+WHERE
+    EMP_NAME = '선동일';    
+
+-- 전체사원의 급여를 기존급여에 25%인상해주기.
+UPDATE EMP_SALARY
+SET
+    SALARY = SALARY * 1.25;
+
+/*
+    UPDATE 시에도 서브쿼리 사용 가능.
+    서브쿼리를 수행한 결과값으로 기존의 값으로부터 변경하겠다.
+    
+    - CREATE시에 서브쿼리 사용함 : 서브쿼리를 수행한 결과를 테이블 만들때 넣어버리겠다.
+    - INSERT시에 서브쿼리 사용함 : 서브쿼리를 수행한 결과를 해당 테이블에 삽입하겠다.
+    
+    [표현법]
+    UPDATE 테이블명
+    SET 컬럼명 = (서브쿼리)
+    WHERE 조건; // 생략가능
+*/
+
+-- EMP_SALARY 테이블에 홍길동 사원의 부서코드를 선동일 사원의 부서코드로 변경.
+-- 홍길동 부서코드 D1, 선동일 부서코드 D9
+-- 1) 선동일 사원의 부서코드를 알아내는 쿼리문
+SELECT
+    DEPT_CODE
+FROM
+    EMPLOYEE
+WHERE
+    EMP_NAME = '선동일';
+-- 2) 홍길동 부서코드를 D9으로 변경
+UPDATE EMP_SALARY
+SET
+    DEPT_CODE = (
+        SELECT
+            DEPT_CODE
+        FROM
+            EMPLOYEE
+        WHERE
+            EMP_NAME = '선동일'
+    )
+WHERE
+    EMP_NAME = '홍길동';
+
+SELECT
+    *
+FROM
+    EMP_SALARY;
+
+-- 방명수 사원의 급여와 보너스를 유재식 사원의 급여와 보너스값으로 변경
+UPDATE EMP_SALARY
+SET
+    SALARY = (
+        SELECT
+            SALARY
+        FROM
+            EMP_SALARY
+        WHERE
+            EMP_NAME = '유재식'
+    ),
+    BONUS = (
+        SELECT
+            BONUS
+        FROM
+            EMP_SALARY
+        WHERE
+            EMP_NAME = '유재식'
+    )
+WHERE
+    EMP_NAME = '방명수';
+
+--SET(SALARY, BONUS) = (SELECT SALARY, BONUS FROM EMP_SALARY WHERE EMP_NAME = '유재식');
+
+-- 송종기 직원의 사번을 200으로 바꾸기.
+UPDATE EMPLOYEE
+SET
+    EMP_ID = 200
+WHERE
+    EMP_NAME = '송종기';
+
+UPDATE EMPLOYEE
+SET
+    EMP_ID = 905
+WHERE
+    EMP_NAME = '선동일';
+
+ROLLBACK;
+
+/*
+    4. DELETE
+    테이블에 기록된 데이터를 "행" 단위로 삭제하는 구문.
+    [표현법]
+    DELETE FROM 테이블명
+    WHERE 조건; -- WHERE절 생략가능. 생략시에는 테이블의 모든 행삭제.
+*/
+
+-- EMPLOYEE테이블의 모든 행 삭제
+DELETE FROM EMPLOYEE;
+
+SELECT
+    *
+FROM
+    EMPLOYEE;
+-- 테이블이 삭제된건 아님
+
+ROLLBACK; -- 롤백시 마지막으로 커밋한시점으로 돌아감.
+
+-- DELETE 문으로 EMPLOYEE테이블안의 홍길동, 민경민 정보를 지우기.
+DELETE FROM EMPLOYEE
+WHERE
+    EMP_NAME IN ( '홍길동', '민경민' );
+
+-- WHERE 절의 조건에따라 1개 이상의행 OR 0개행이 변경이 될수 있다.
+COMMIT;
+
+-- DEPARTMENT테이블로부터 DEPT_ID가 D1인 부서 삭제
+DELETE FROM DEPARTMENT
+WHERE
+    DEPT_ID = 'D1';
+-- 만약에 EMPLOYEE테이블의 DEPT_CODE컬럼에서 외래키로 DEPT_ID칼럼을 참조하고있을경우
+-- 삭제가 되지 않앗을것.
+
+ROLLBACK;
+
+/*
+    TRUNCATE : 테이블의 전체행을 모두 삭제할때 사용하는 구문
+                DELETE구문보다 수행속도가 매우빠름
+                별도의 조건을 제시 불가
+                ROLLBACK이 불가.
+    
+    [표현법]
+    TRUNCATE TABLE 테이블명;
+        
+        TRUNCATE TABLE 테이블명;                ||                  DELETE FROM 테이블명
+==============================================================================================
+        별도의 조건제시 불가                                         특정조건 제시가능
+        수해속도빠름                                                수행속도 느림
+        ROLLBACK 불가                                              ROLLBACK 가능.
+*/
+
+SELECT
+    *
+FROM
+    EMP_SALARY;
+
+DELETE FROM EMP_SALARY;
+
+ROLLBACK;
+
+TRUNCATE TABLE EMP_SALARY;
---
+++ SQL03_SELECT(Option).sql
@@ -0,0 +1,176 @@
+-- 1
+-- 학생이름과 주소지를 표시 헤더 "학생 이름", "주소지" 이름으로 오름차순
+SELECT
+    STUDENT_NAME    "학생 이름",
+    STUDENT_ADDRESS 주소지
+FROM
+    TB_STUDENT
+ORDER BY
+    1 ASC;
+
+-- 2
+-- 휴학중인 학생들의 이름과 주민번호 나이가 적은순서로 출력
+SELECT
+    STUDENT_NAME,
+    STUDENT_SSN
+FROM
+    TB_STUDENT
+WHERE
+    ABSENCE_YN = 'Y'
+ORDER BY
+    2 DESC;
+
+-- 3
+-- 주소지가 경기도, 강원도인 학생들중 1900년대 학번을 가진 학생들 이름 학번 주소 이름의 오름차순으로 출력
+-- 출력 헤더 "학생이름", "학번", "거주지 주소"
+SELECT
+    STUDENT_NAME    학생이름,
+    STUDENT_NO      학번,
+    STUDENT_ADDRESS "거주지 주소"
+FROM
+    TB_STUDENT
+WHERE
+    ( STUDENT_ADDRESS LIKE '경기%'
+      OR STUDENT_ADDRESS LIKE '강원%' )
+    AND SUBSTR(STUDENT_NO, 1, 1) != 'A'
+ORDER BY
+    1;
+
+-- 4
+-- 법학과 교수 중 가장 나이가 많은 사람부터 출력 법학과의 '학과코드' 학과 테이블을 조회
+SELECT
+    PROFESSOR_NAME,
+    PROFESSOR_SSN
+FROM
+    TB_PROFESSOR
+WHERE
+    DEPARTMENT_NO = '005'
+ORDER BY
+    2;
+
+-- 5
+-- 2004년 2학기에 'C3118100' 과목 수강한 학생들 학점 조회 학점이 높은 학생부터 표시하고 
+-- 학점이 같으면 학번이 낮은 학생부터 표시
+SELECT
+    STUDENT_NO,
+    POINT
+FROM
+    TB_GRADE
+WHERE
+        CLASS_NO = 'C3118100'
+    AND TERM_NO = '200402'
+ORDER BY
+    2 DESC,
+    STUDENT_NO;
+
+-- 6
+-- 학생 번호, 이름, 학과 이름을 학생 이름으로 오름차순 정렬
+SELECT
+    STUDENT_NO,
+    STUDENT_NAME,
+    DEPARTMENT_NAME
+FROM
+         TB_STUDENT
+    JOIN TB_DEPARTMENT USING ( DEPARTMENT_NO )
+ORDER BY
+    2 ;
+
+-- 7
+-- 과목 이름과 과목의 학과 이름을 출력
+SELECT
+    CLASS_NAME,
+    DEPARTMENT_NAME
+FROM
+         TB_CLASS
+    JOIN TB_DEPARTMENT USING ( DEPARTMENT_NO );
+
+-- 8
+-- 과목병 교수 이름을 찾는다. 과목 이름과 교수 이름을 출력
+SELECT
+    C.CLASS_NAME,
+    PROFESSOR_NAME
+FROM
+         TB_PROFESSOR A
+    JOIN TB_CLASS_PROFESSOR B ON A.PROFESSOR_NO = B.PROFESSOR_NO
+    JOIN TB_CLASS           C ON B.CLASS_NO = C.CLASS_NO
+ORDER BY
+    TB_CLASS.DEPARTMENT_NO ASC, 2 ASC;               
+
+-- 9
+-- 8번의 결과 중 '인문사회' 계열에 속한 과목의 교수 이름을 찾는다. 이에 해당하는 과목 이름과 교수 이름 출력
+SELECT
+    C.CLASS_NAME,
+    PROFESSOR_NAME
+FROM
+         TB_PROFESSOR A
+    JOIN TB_CLASS_PROFESSOR B ON A.PROFESSOR_NO = B.PROFESSOR_NO
+    JOIN TB_CLASS           C ON B.CLASS_NO = C.CLASS_NO
+    JOIN TB_DEPARTMENT      D ON C.DEPARTMENT_NO = D.DEPARTMENT_NO
+WHERE
+    D.CATEGORY = '인문사회'
+ORDER BY
+    2;
+
+-- 10
+-- '음악학과' 학생들의 평점 구하려 한다. 음악학과 학생들의 "학번", "학생 이름", "전체 평점"을 출력
+-- 평점은 소수점 1자리까지만 반올림하여 표시
+
+-- 11
+-- 학번이 A313047 인 학생이 학교에 나오고 있지 않다. 지도 교수에게 내용을 전달하기 위한 학과 이름, 학생이름과 지도 교수
+-- 이름이 필요하다. 출력헤더 "학과이름", "학생이름", "지도교수이름"
+
+-- 12
+-- 2007 년도에 '인간관계론' 과목을 수강한 학생을 찾아 학생 리므과 수강학기 표시
+
+-- 13
+-- 예체능 계열 과목 중 과목 담당교수를 한 명도 배정받지 못한 과목을 찾아 그 과목 이름과 학과 이름 출력
+
+-- 14
+-- 서반아어학과 학생들의 지도교수를 게시하려한다. 학생이름과 지도교수 이름을 찾고 만일 지도 교수가 없는 학생일 경우
+-- "지도교수 미지정"으로 표시하도록 한다. 출력헤더 "학생이름", "지도교수"로 표시하며 고학번 학생이 먼저 표시되도록 한다.
+
+-- 15
+-- 휴학생이 아닌 학생 중 평점이 4.0 이상인 학생을 찾아 그 학생의 학번, 이름, 학과이름, 평점을 출력
+
+-- 16
+-- 환경조경학과 전공과목들의 과목 별 평점을 파악할 수 있는 구문
+
+-- 17
+-- 재학중인 최경희 학생과 같은 과 학생들의 이름과 주소를 출력
+
+-- 18
+-- 국어국문학과에서 총 평점이 가장 높은 학생의 이름과 학번을 표시
+
+SELECT STUDENT_NO, STUDENT_NAME FROM(
+SELECT STUDENT_NO, STUDENT_NAME, AVG(POINT) AS POINT
+FROM TB_STUDENT
+JOIN TB_GRADE USING(STUDENT_NO)
+WHERE DEPARTMENT_NO = (SELECT DEPARTMENT_NO 
+                        FROM TB_DEPARTMENT
+                        WHERE DEPARTMENT_NAME = '국어국문학과'
+                        )
+GROUP BY STUDENT_NO, STUDENT_NAME
+ORDER BY POINT DESC
+)
+WHERE ROWNUM = 1;
+-------------------------------------------------------------
+SELECT STUDENT_NO, STUDENT_NAME
+FROM TB_STUDENT
+JOIN TB_GRADE USING(STUDENT_NO)
+WHERE DEPARTMENT_NO = (SELECT DEPARTMENT_NO 
+                        FROM TB_DEPARTMENT
+                        WHERE DEPARTMENT_NAME = '국어국문학과'
+                        )
+GROUP BY STUDENT_NO, STUDENT_NAME
+HAVING AVG(POINT) = ( 
+                        SELECT MAX(AVG(POINT))
+                        FROM TB_STUDENT
+                        JOIN TB_GRADE USING(STUDENT_NO)
+                        WHERE DEPARTMENT_NO = '001'
+                        GROUP BY STUDENT_NO                    
+                    );
+
+
+-- 19
+-- "환경조경학과"가 속한 같은 계열 학과들의 학과 별 전공과목 평점을 파악하기 위한 적절한 구문 찾아내기
+-- 출력헤더 "계열 학과명", "정공평점"으로 표시, 평점은 소수점 한 자리까지만 반올림하여 표시.
